154834086abb5ab5115495e5914681aef0ac1d64
Nicolas Léveillé <nicolas@uucidl.com>
2012-07-22 13:50:02 +0200

    store allocated segments in a growable array

--- src/aqp.c
+++ src/aqp.c
@@ -1,26 +1,65 @@
 #include "alloc.h"
 #include "aqp.h"
 
-typedef struct aqpPiece { char a; } Piece;
-typedef struct aqpSegment {} Segment;
-typedef struct aqpSegmentEditionRange {} SegmentEditionRange;
-typedef struct aqpSegmentRange {} SegmentRange;
+#include "growa.h"
+
+typedef struct aqpSegment
+{
+
+} Segment;
+
+typedef struct aqpSegmentRange
+{
+} SegmentRange;
+
+typedef struct aqpSegmentEditionRange
+{
+} SegmentEditionRange;
+
+typedef struct aqpPiece
+{
+	Segment** segments;
+	int segments_n;
+	int segments_size;
+} Piece;
 
 extern Piece* aqp_new_piece(void)
 {
 	Piece* piece = aqp_calloc(1, sizeof *piece);
+
+	piece->segments   = 0;
+	piece->segments_n = 0;
+	piece->segments_size = 0;
 
 	return piece;
 }
 
 extern void aqp_delete_piece(Piece* piece)
 {
+	for (int i = 0; i < piece->segments_n; i++) {
+		aqp_free (piece->segments[i]);
+		piece->segments[i] = 0;
+	}
+	freea_m(piece->segments, piece->segments_n, piece->segments_size);
 	aqp_free (piece);
 }
 
 extern Segment* aqp_new_segment (Piece* piece)
 {
-	return 0;
+	int i;
+	for (i = 0; i < piece->segments_n; i++) {
+		if (!piece->segments[i]) {
+			break;
+		}
+	}
+
+	growa_m(i + 1, piece->segments, piece->segments_n, piece->segments_size);
+
+	Segment* segment = aqp_calloc (1, sizeof *segment);
+
+	piece->segments[i] = segment;
+
+	return segment;
 }
 
 extern SegmentEditionRange* aqp_edit_range (Segment* segment, int start, int end)
---
+++ src/growa.h
@@ -0,0 +1,25 @@
+#ifndef growa_h
+#define growa_h
+
+/**! resize array until we reach n elements */
+#define growa_m(n_expr, array_var, elementn_var, arrayn_var) do {	\
+		int const n = n_expr;					\
+		if (arrayn_var < n) {					\
+			if (arrayn_var <= 0) {				\
+				arrayn_var = 1;				\
+			}						\
+			while (arrayn_var < n) {			\
+				arrayn_var *= 2;			\
+			}						\
+			array_var = aqp_realloc (array_var, arrayn_var * sizeof (*array_var)); \
+		}							\
+		elementn_var = n;					\
+	} while (0)
+
+#define freea_m(array_var, elementn_var, arrayn_var) do {	\
+		aqp_free (array_var);				\
+		array_var = 0;					\
+		arrayn_var = elementn_var = 0;			\
+	} while(0)						\
+
+#endif
